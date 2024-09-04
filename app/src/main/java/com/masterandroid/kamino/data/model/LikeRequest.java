91eb28a4de69993d1ab6a1a55eb8b3fe3d0a1d9c
nebojsaadamovic <adamovic.nebojsa@gmail.com>
2024-09-04 18:35:02 +0200

    * added LikeRequest class

---
+++ app/src/main/java/com/masterandroid/kamino/data/model/LikeRequest.java
@@ -0,0 +1,12 @@
+package com.masterandroid.kamino.data.model;
+
+
+
+public class LikeRequest {
+    private int planetId;
+
+    public LikeRequest(int planetId) {
+        this.planetId = planetId;
+    }
+
+}
