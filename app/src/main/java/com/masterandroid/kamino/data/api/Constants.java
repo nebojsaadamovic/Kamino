714cb95c69d9be1e9be0d0a6912bdd5df6325dd0
nebojsaadamovic <adamovic.nebojsa@gmail.com>
2024-09-17 01:08:25 +0200

    * update api url

--- app/src/main/java/com/masterandroid/kamino/data/api/Constants.java
+++ app/src/main/java/com/masterandroid/kamino/data/api/Constants.java
@@ -1,6 +1,18 @@
 package com.masterandroid.kamino.data.api;
+import android.app.Application;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.net.NetworkInfo;
+import android.os.Build;
+import android.util.Log;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 
 public class Constants {
 
-    public static final String BASE_URL = "https://private-anon-59e7f97310-starwars2.apiary-mock.com/";
+       public static final String BASE_URL = "https://private-anon-9bf14e01b4-starwars2.apiary-mock.com/";
 }
